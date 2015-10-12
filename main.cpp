161245499f1644659267f4075bd2fb101066eed7
kaltu <lkaxtu@gmail.com>
2015-10-12 23:29:27 +0800

    結構整理
    調整目前的function call結構
    以便新增新功能
    replace()需要被完成

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ using namespace std ;
 #include "Sudoku.hpp"
 
 int main( int argc, char** argv ) {
-    srand(time(NULL));
-    Sudoku sudoku(9);
+    srand(time(NULL)) ;
+    Sudoku sudoku(9) ;
     sudoku.print_sudoku() ;
+    cout << "build in " << sudoku.times() << " times." ;
     return 0 ;
 } // main
-
-
