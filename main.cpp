b0f5cf8132e9fc7efc6774544d75b13aeeb70a6b
sqaz91819 <z390124578@gmail.com>
2015-10-13 00:32:16 +0900

    Revert "結構整理"
    This reverts commit 161245499f1644659267f4075bd2fb101066eed7.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ using namespace std ;
 #include "Sudoku.hpp"
 
 int main( int argc, char** argv ) {
-    srand(time(NULL)) ;
-    Sudoku sudoku(9) ;
+    srand(time(NULL));
+    Sudoku sudoku(9);
     sudoku.print_sudoku() ;
-    cout << "build in " << sudoku.times() << " times." ;
     return 0 ;
 } // main
+
+
