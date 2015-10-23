bea28bb62eadccbb1d235fdf2d7e294ac2645b9c
kaltu <lkaxtu@gmail.com>
2015-10-23 23:00:59 +0800

    UI update

--- main.cpp
+++ main.cpp
@@ -18,20 +18,20 @@ void play( Manager &manager ) {
     manager.clear_screen() ;
     Player player( manager.request_quesion() ) ;
     manager.clear_screen() ;
-    cout << "1. Exercise" << endl
-         << "2. Advantage" << endl
-         << "3. Show Ranking" << endl
-         << "0. exit player mode" << endl
+    cout << "1. P)ractice" << endl
+         << "2. A)dvanture" << endl
+         << "3. Show R)anking" << endl
+         << "0. E)xit player mode" << endl
          << ">>> " ;
     string opt ;
     cin >> opt ;
-    if ( opt == "0" )
+    if ( opt == "0" || opt == "E" || opt == "e" )
         return start( manager ) ;
-    else if ( opt == "1" )
+    else if ( opt == "1" || opt == "P" || opt == "p" )
         player.exerciseMode() ;
-    else if ( opt == "2" )
+    else if ( opt == "2" || opt == "A" || opt == "a" )
         player.stageMode( manager ) ;
-    else if ( opt == "3" )
+    else if ( opt == "3" || opt == "R" || opt == "r" )
         manager.print_rank() ;
     else return play( manager ) ;
     system( "pause" ) ;
@@ -40,31 +40,32 @@ void play( Manager &manager ) {
 
 void manage( Manager &manager ) {
     manager.clear_screen() ;
-    cout << "1. show all sudokus" << endl
-         << "2. show current sudoku" << endl
-         << "3. change sudoku" << endl
-         << "4. add a new sudoku" << endl
-         << "5. delete current sudoku" << endl
-         << "0. exit managing mode" << endl
+    cout << "1. show A)ll sudokus" << endl
+         << "2. show C)urrent sudoku" << endl
+         << "3. S)witch sudoku" << endl
+         << "4. add a N)ew sudoku" << endl
+         << "5. D)elete current sudoku" << endl
+         << "0. E)xit managing mode" << endl
          << ">>> " ;
     string opt ;
     cin >> opt ;
-    if ( opt == "1" )
+    manager.clear_screen() ;
+    if ( opt == "1" || opt == "A" || opt == "a" )
         manager.print_sudokus() ;
-    else if ( opt == "2" )
+    else if ( opt == "2" || opt == "C" || opt == "c" )
         manager.print_sudoku() ;
-    else if ( opt == "3" ) {
+    else if ( opt == "3" || opt == "S" || opt == "s" ) {
         manager.print_sudokus() ;
         int index ;
         cout << "Whitch one do you wnat to switch to?" << endl << ">>> " ;
         cin >> index ;
         manager.switch_sudoku( index ) ;
     }
-    else if ( opt == "4" )
+    else if ( opt == "4" || opt == "N" || opt == "n" )
         manager.create_sudoku() ;
-    else if ( opt == "5" )
+    else if ( opt == "5" || opt == "D" || opt == "d" )
         manager.delete_sudoku() ;
-    else if ( opt == "0" )
+    else if ( opt == "0" || opt == "E" || opt == "e" )
         return start( manager ) ;
     else return manage( manager ) ;
     system( "pause" ) ;
@@ -73,18 +74,18 @@ void manage( Manager &manager ) {
 
 void start( Manager &manager ) {
     manager.clear_screen() ;
-    cout << "1. play" << endl
-         << "2. manage" << endl
-         << "0. exit" << endl
+    cout << "1. P)lay" << endl
+         << "2. M)anage" << endl
+         << "0. E)xit" << endl
          << ">>> " ;
     string opt ;
 
     cin >> opt ;
-    if ( opt == "1" )
+    if ( opt == "1" || opt == "P" || opt == "p" )
         return play( manager ) ;
-    else if ( opt == "2" )
+    else if ( opt == "2" || opt == "M" || opt == "m" )
         return manage( manager ) ;
-    else if ( opt == "0" )
+    else if ( opt == "0" || opt == "E" || opt == "e" )
         return init() ;
     else
         return start( manager ) ;
@@ -92,21 +93,21 @@ void start( Manager &manager ) {
 
 void init(void) {
     system( "cls" ) ;
-    cout << "4. mini-sudoku" << endl
-         << "9. standard-sudoku" << endl
-         << "0. exit" << endl
+    cout << "4. M)ini-sudoku" << endl
+         << "9. S)tandard-sudoku" << endl
+         << "0. E)xit" << endl
          << ">>> " ;
     string i ;
     cin >> i ;
-    if ( i == "4" ) {
+    if ( i == "4" || i == "M" || i == "m" ) {
         Manager manager(4) ;
         start( manager ) ;
     }
-    else if ( i == "9" ) {
+    else if ( i == "9" || i == "S" || i == "s" ) {
         Manager manager(9) ;
         start( manager ) ;
     }
-    else if ( i != "0" )
+    else if ( i != "0" && i != "E" && i != "e" )
         return init() ;
 }
 
